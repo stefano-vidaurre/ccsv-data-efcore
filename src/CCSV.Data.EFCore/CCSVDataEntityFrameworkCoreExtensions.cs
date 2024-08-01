b779d7e594afe180d3dcff07e9423c880567d482
Stefano Vidaurre Olite <fano494@gmail.com>
2024-08-02 00:21:38 +0200

    feat: add "use utc date time converter" extension method

--- src/CCSV.Data.EFCore/CCSVDataEntityFrameworkCoreExtensions.cs
+++ src/CCSV.Data.EFCore/CCSVDataEntityFrameworkCoreExtensions.cs
@@ -1,12 +1,25 @@
 using CCSV.Data.EFCore.Middlewares;
 using CCSV.Domain.Repositories;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CCSV.Data.EFCore;
 
 public static class CCSVDataEntityFrameworkCoreExtensions
 {
+    private static ValueConverter<DateTime, DateTime> DateTimeConverter = 
+        new ValueConverter<DateTime, DateTime>(
+            v => v.ToUniversalTime(),
+            v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+    
+    private static ValueConverter<DateTime?, DateTime?> NullableDateTimeConverter = 
+        new ValueConverter<DateTime?, DateTime?>(
+            v => v.HasValue ? v.Value.ToUniversalTime() : v,
+            v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : v);
+
     public static IServiceCollection AddEntityFramework(this IServiceCollection services)
     {
         services.AddScoped<ITransaction, Transaction>();
@@ -18,5 +31,38 @@ public static class CCSVDataEntityFrameworkCoreExtensions
     public static IApplicationBuilder UseUnitOfWork(this IApplicationBuilder builder)
     {
         return builder.UseMiddleware<UnitOfWorkMiddleware>();
+    }
+
+    public static void UseUTCDateTimeConverter(this ModelBuilder modelBuilder)
+    {
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            SetConverterToEntity(entityType);
+        }
+    }
+
+    private static void SetConverterToEntity(IMutableEntityType entityType)
+    {
+        if (entityType.IsKeyless)
+        {
+            return;
+        }
+
+        foreach (var property in entityType.GetProperties())
+        {
+            SetConverterToProperty(property);
+        }
+    }
+
+    private static void SetConverterToProperty(IMutableProperty property)
+    {
+        if (property.ClrType == typeof(DateTime))
+        {
+            property.SetValueConverter(DateTimeConverter);
+        }
+        else if (property.ClrType == typeof(DateTime?))
+        {
+            property.SetValueConverter(NullableDateTimeConverter);
+        }
     }
 }
